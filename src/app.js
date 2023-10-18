c39b8b580baa28d7cc0138d733faea12068fcacb
Odili1 <ezedigwegerald1@gmail.com>
2023-10-18 01:06:51 +0100

    Updated folder

--- src/app.js
+++ src/app.js
@@ -1,44 +1,42 @@
 const express = require("express");
-const sequelize = require('./database/sequelize');
-const userRoutes = require('./users/user.routes');
-const productsRoutes = require('./products/products.routes');
-require('dotenv').config();
+const sequelize = require("./database/sequelize");
+const userRoutes = require("./Users/user.routes");
+const productsRoutes = require("./products/products.routes");
+require("dotenv").config();
 
 // Intialize App
 const app = express();
 
 // Port
-const port = process.env.PORT
+const port = process.env.PORT;
 
 // Express Middleware
 app.use(express.json());
 
-
 // Routes
-app.use('/users', userRoutes);
-app.use('/products', productsRoutes);
-
+app.use("/users", userRoutes);
+app.use("/products", productsRoutes);
 
 // Uncaught Route
-app.get('*', (req, res) => {
-    console.log("404");
-    res.status(404).json({
-        data: null,
-        error: 'Route not found'
-    })
-})
-
+app.get("*", (req, res) => {
+  console.log("404");
+  res.status(404).json({
+    data: null,
+    error: "Route not found",
+  });
+});
 
 // Testing DB connection
-sequelize.authenticate().then(() => {
+sequelize
+  .authenticate()
+  .then(() => {
     console.log(`Database connected successfully`);
-}).catch((error) => {
+  })
+  .catch((error) => {
     console.log(`An error occured while connecting to database`);
-})
+  });
 
 // Server
-app.listen(port, ()=> {
-    console.log(`App started on port ${port}`);
-})
-
-
+app.listen(port, () => {
+  console.log(`App started on port ${port}`);
+});
